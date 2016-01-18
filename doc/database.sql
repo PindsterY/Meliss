3b5895a5a0e2ba3aef65322c710e4948d86d450d
alvarpungar <alvar.pungar@khk.ee>
2016-01-18 11:21:24 +0200

    Halo template [Delivers #111862677]

--- doc/database.sql
+++ doc/database.sql
@@ -1,105 +1,43 @@
--- MySQL dump 10.16  Distrib 10.1.9-MariaDB, for Win32 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: meliss
--- ------------------------------------------------------
--- Server version	10.1.9-MariaDB
+-- Host: localhost:3306
+-- Loomise aeg: Sept 29, 2014 kell 03:59 PM
+-- Serveri versioon: 5.5.34
+-- PHP versioon: 5.5.10
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 --
--- Table structure for table `openday`
+-- Andmebaas: `halo`
 --
 
-DROP TABLE IF EXISTS `openday`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `openday` (
-  `open_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `open_dayname` varchar(255) NOT NULL,
-  `open_daynum` int(10) unsigned NOT NULL,
-  `open_opened` tinyint(4) NOT NULL,
-  PRIMARY KEY (`open_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `openday`
+-- Tabeli struktuur tabelile `user`
 --
 
-LOCK TABLES `openday` WRITE;
-/*!40000 ALTER TABLE `openday` DISABLE KEYS */;
-/*!40000 ALTER TABLE `openday` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order`
---
-
-DROP TABLE IF EXISTS `order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `order` (
-  `order_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `first_dish` int(10) unsigned NOT NULL,
-  `main_dish` int(10) unsigned NOT NULL,
-  `last_dish` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order`
---
-
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reserv`
---
-
-DROP TABLE IF EXISTS `reserv`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reserv` (
-  `reserv_id` int(10) unsigned NOT NULL,
-  `phone` varchar(255) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `is_admin` tinyint(4) NOT NULL DEFAULT '0',
+  `password` varchar(255) NOT NULL,
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `email` varchar(255) NOT NULL,
-  `seats` int(10) unsigned NOT NULL,
-  `order_id` int(10) unsigned NOT NULL,
-  `cancelled` tinyint(3) unsigned NOT NULL,
-  `email_sent` tinyint(3) unsigned NOT NULL,
-  `date_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UNIQUE` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `reserv`
+-- Andmete tõmmistamine tabelile `user`
 --
 
-LOCK TABLES `reserv` WRITE;
-/*!40000 ALTER TABLE `reserv` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reserv` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-01-18 11:14:08
+INSERT INTO `users` (`user_id`, `username`, `is_admin`, `password`, `active`, `email`, `deleted`) VALUES
+  (1, 'demo', 0, 'demo', 1, '', 0);
+SET FOREIGN_KEY_CHECKS=1;
